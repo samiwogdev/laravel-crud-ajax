980a8e943a9b0772806890fe485e2259d69775c9
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-08-02 14:11:11 +0100

    Loading the staff model, controller and the index view

--- database/migrations/2023_08_02_121430_create_staff_table.php
+++ database/migrations/2023_08_02_121430_create_staff_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('staff', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('level');
+            $table->string('pic');
             $table->timestamps();
         });
     }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD App Laravel & Ajax</title>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css' />
+    <link rel='stylesheet'
+        href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css' />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.css" />
+
+</head>
+
+<!-- Fonts -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.min.js'></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+
+</html>
